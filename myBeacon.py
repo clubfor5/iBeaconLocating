f35121a57cff2a94836b1d9b44d40b69c50bd0da
clubfor5 <444305475@qq.com>
2018-12-27 03:38:10 +0000

    A raw data pushed

--- myBeacon.py
+++ myBeacon.py
@@ -1,11 +1,12 @@
 from bluepy.btle import Scanner, DefaultDelegate
 import urllib3
 import time
+import json
 #from requests_futures.sessions import FuturesSession as FS
 
 
 locked = False
-beacons = ['12:3b:6a:1b:90:a2','f9:70:db:a9:7c:7a','DB:D5:1D:C1:F3:49','db:d5:1d:c1:f3:49', 'FF:D8:B1:6B:5E:9D','ff:d8:b1:6b:5e:9d','DF:67:4F:0F:DA:8D','df:67:4f:0f:da:8d']
+beacons = ['12:3b:6a:1b:90:98','12:3b:6a:1b:94:2d','12:3b:6a:1b:8e:08','12:3b:6a:1b:8e:06','12:3b:6a:1b:97:f5', '12:3b:6a:1b:8f:64','12:3b:6a:1b:93:52','12:3b:6a:1b:8e:96','12:3b:6a:1b:87:ce', '12:3b:6a:1b:90:9f']
 
 
 class ScanDelegate(DefaultDelegate):
@@ -45,24 +46,44 @@ scanner = Scanner().withDelegate(ScanDelegate())
 #=========#=====================#==========#======================#=========
 def beaconScanner():
     count = 0
+    tick = 0
     loss = 0
+    total = 0
+    myFile = open("iBeaconRawData " + time.asctime(time.localtime(time.time())) + ".txt", 'w')
     while True:
-        count = count + 1
+        currentTime = time.time()
         myDevice = False
         global lastSended, scanner,locked
-        devices = scanner.scan(0.5) #insert a time to timeout inside the squares. this returns a list with ALL bluetooth devices nearby (not only BLE).
+        devices = scanner.scan(0.47) #insert a time to timeout inside the squares. this returns a list with ALL bluetooth devices nearby (not only BLE).
         for dev in devices:
             if not beacons.__contains__(dev.addr): #first of all check if the device at this position is or not one of ours beacons. if not, we just continue the loop, passing to next interaction.
                 continue
 
             elif beacons.__contains__(dev.addr):
                 myDevice = True
-                print(dev.addr)
-                print(dev.rssi)
-        if myDevice == False:
+                myFile.writelines(json.dumps({"addr": dev.addr, "RSSI": dev.rssi, "time": currentTime}, sort_keys = True, indent = 4, separators=(',', ':')) + "\n")
+                
+                count = count + 1
+        tick = tick + 1
+        if myDevice == True:
+            print("Received "),
+            print(count),
+            print(" packets in time"),
+            print(currentTime)
+            total = total + count
+            print("Total: "),
+            print(total),
+            print(" Tick: "),
+            print(tick),
+            print(" Average: "),
+            print(total / tick)
+            count = 0
+        else:
             loss = loss + 1
-            print("packet loss: "),
+            print("packet loss"),
             print(loss),
-            print("/"),
-            print(count)
+            print("int time"),
+            print(currentTime)
+            count = 0
+        print("")
 beaconScanner()
