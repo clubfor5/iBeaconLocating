a511f7869b1b0726fc3a5734837193d44698f2e8
clubfor5 <444305475@qq.com>
2019-03-03 13:05:10 +0000

    cao

--- positioning/purePI_ctlersp.py
+++ positioning/purePI_ctlersp.py
@@ -10,14 +10,14 @@ class purePI_Ctler:
         self.skp = skp
         self.ski = ski
         self.skpr = skpr
-        self.__uBuffer = 0       #上一时刻的控制器输入
-        self.__time = [0 for i in range(8)]  #用于平滑
+        self.__uBuffer = 0       #
+        self.__time = [0 for i in range(8)]  #
         self.output = 0
-        self.uk = 0      #现在时刻的controller输入
-        self.speed = 0    #当前输出速度
-        self.__uspeed = 0 #当前速度输入量
+        self.uk = 0      #
+        self.speed = 0    #
+        self.__uspeed = 0 #
         self.__uspeed1 = 0
-        self.__speedIBuffer = 0    #上一时刻速度输入量
+        self.__speedIBuffer = 0    #
         self.__outputBuffer = [start_posi for i in range(8)] 
         self.__speedBuffer = [0 for i in range(8)]
     
@@ -46,19 +46,19 @@ class purePI_Ctler:
 
 #####################################
 #demo
-'''
-f = np.loadtxt("/Users/siriushe/Documents/MATLAB/newdata2.txt",delimiter = ',')
-ctler = purePI_Ctler(f[0][1]) #初始化时输入位置信息
-output = [0 for i in range(len(f[:][:]))] #为方便输出图像用的list nevermind
-speed = [0 for i in range(len(output))]  #同上
-output[0] = f[0][1]          #初始信息
-speed[0] = 0            #同上
-for i in range(len(output)-1):   #每次把位置&时间输入进去
-    [output[i+1],speed[i+1]] = ctler.posiFlter(f[i+1][1],f[i+1][0])
-plt.figure
+if  __name__ == "__main__":
+	f = np.loadtxt("/Users/siriushe/Documents/MATLAB/newdata2.txt",delimiter = ',')
+	ctler = purePI_Ctler(f[0][1]) #
+	output = [0 for i in range(len(f[:][:]))] #list nevermind
+	speed = [0 for i in range(len(output))]  #
+	output[0] = f[0][1]          #
+	speed[0] = 0            #
+	for i in range(len(output)-1):   #
+    		[output[i+1],speed[i+1]] = ctler.posiFlter(f[i+1][1],f[i+1][0])
+	plt.figure
 #plt.plot(f[:,0],f[:,2])
 #plt.plot(f[:,0],speed)
-plt.plot(f[:,0],output)
-plt.savefig('/Users/siriushe/Desktop/abcdd.png')
-'''
+	plt.plot(f[:,0],output)
+	plt.savefig('/Users/siriushe/Desktop/abcdd.png')
+
 
